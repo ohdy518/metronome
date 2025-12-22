a52f3d3ac3dc61488ef29f17584ae964db2fbf67
ohdy518 <ohdy518@gmail.com>
2025-12-22 23:51:34 +0900

    Upgrade.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+    import {Play, Square, RotateCcw, SlidersVertical} from '@lucide/svelte'
+    import {Pane} from 'tweakpane';
+    import {browser} from "$app/environment";
 
     // objects and auto-sets
     let loopEntity;
@@ -6,7 +9,38 @@
     let timer;
     let beat = 1;
     let bar = 1;
+    let started = false;
 
+    const PARAMS = {
+        beat: 1,
+        bar: 1,
+        started: false,
+        bpm: 180,
+        beatsPerBar: 4,
+    }
+
+    let pane;
+
+    if (browser) {
+        pane = new Pane({
+            title: "Configurations",
+            container: document.getElementById('config-container'),
+        })
+        const f1 = pane.addFolder({
+            title: 'Status',
+        })
+        f1.addBinding(PARAMS, 'beat', {min: 1, step: 1})
+        f1.addBinding(PARAMS, 'bar', {min: 1, step: 1})
+        f1.addBinding(PARAMS, 'started', {readonly: true})
+        const f2 = pane.addFolder({
+            title: 'Setup',
+        })
+        f2.addBinding(PARAMS, 'bpm', {min: 1})
+        f2.addBinding(PARAMS, 'beatsPerBar', {min: 1, step: 1})
+
+        // pane.style.fontFamily = "JetBrains Mono"
+        pane.hidden = true
+    }
     // configs
     let updateResolution = 1
 
@@ -14,29 +48,42 @@
     let bpm = 180
     let beatsPerBar = 4
 
-
     function start() {
+        if (PARAMS.started) {return}
+        PARAMS.started = true;
         init()
+        startTime = Date.now()
+        timer = Date.now()
         loopEntity = setInterval(() => {loop()}, updateResolution);
     }
 
     function stop() {
+        PARAMS.started = false;
         clearInterval(loopEntity);
     }
 
+    function reset() {
+        stop()
+        init()
+    }
+
     function init() {
-        startTime = Date.now()
-        timer = Date.now()
+        PARAMS.beat = 1;
+        PARAMS.bar = 1;
     }
 
     function loop() {
         let timeElapsed = Date.now() - timer
-        if (timeElapsed >= (60/bpm*1000)) {
-            beat++;
+        if (timeElapsed >= (60/PARAMS.bpm*1000)) {
+            PARAMS.beat++;
 
-            if (beat > beatsPerBar) { beat -= beatsPerBar; bar++; }
+            if (PARAMS.beat > PARAMS.beatsPerBar) { PARAMS.beat -= PARAMS.beatsPerBar; PARAMS.bar++; }
             timer = Date.now()
         }
+    }
+
+    function setting() {
+        pane.hidden = !(pane.hidden)
     }
 </script>
 
@@ -46,19 +93,33 @@
          absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
          p-[4vw]
          inter num-style bg-zinc-900 text-zinc-50">
-    <button id="play-control" on:click={start}>Start</button>
-    <button id="play-control" on:click={stop}>Stop</button>
+    <div
+            class="flex flex-row w-fit
+            absolute top-1/4 transform -translate-y-[100%]
+            gap-2 text-zinc-50">
+        <button id="start-button" class="group border-2 border-transparent hover:border-zinc-300 p-1.5" on:click={start}><Play class="group-hover:text-emerald-400"/></button>
+        <button id="stop-button" class="group border-2 border-transparent hover:border-zinc-300 p-1.5" on:click={stop}><Square class="group-hover:text-rose-400"/></button>
+        <button id="stop-button" class="group border-2 border-transparent hover:border-zinc-300 p-1.5" on:click={reset}><RotateCcw class="group-hover:text-amber-400"/></button>
+        <button id="stop-button" class="group border-2 border-transparent hover:border-zinc-300 p-1.5" on:click={setting}><SlidersVertical class="group-hover:text-sky-400"/></button>
+    </div>
+
+    <div id="config-container"
+         class="absolute top-1/4 right-1/4 jbm">
+
+    </div>
+
     <div
             class="flex flex-col w-fit
             absolute top-1/2 transform -translate-y-1/2
             gap-3">
-        <span id="bar" class="font-regular text-4xl text-zinc-200">Bar <span class="font-medium text-zinc-50">{bar}</span></span>
-        <span id="beat" class="font-semibold text-9xl">{beat}<span class="font-regular text-4xl text-zinc-200">&nbsp;/&nbsp;4</span></span>
+        <span id="bar" class="font-regular text-4xl text-zinc-300">Bar <span class="font-medium text-zinc-50">{PARAMS.bar}</span></span>
+        <span id="beat" class="font-semibold text-9xl">{PARAMS.beat}<span class="font-regular text-4xl text-zinc-300">&nbsp;/&nbsp;{PARAMS.beatsPerBar}</span></span>
     </div>
 
     <div
-            class="flex flex-col">
-        <span>This</span>
+            class="flex flex-col w-fit
+            absolute top-3/4">
+        <span class="font-regular text-3xl text-zinc-300"><span class="font-medium text-zinc-50">{PARAMS.bpm}</span> BPM &middot; <span class="font-medium text-zinc-50">A&flat;m</span></span>
     </div>
 
     <div></div>
